199f255656d7a2d13e0571c60defc9212c637fac
Dan <grigoredanandrei@yahoo.com>
2019-03-25 23:09:50 +0200

    update language

--- src/main/scala/Utils/Language.scala
+++ src/main/scala/Utils/Language.scala
@@ -5,15 +5,15 @@ object Language {
     "else", "enum", "extern", "float", "for", "goto", "if", "int", "long", "register", "return", "short", "'signed",
     "sizeof", "static", "struct", "switch", "typedef", "union", "unsigned", "void", "volatile", "while", "import")
 
-  val operators: List[Char] = List(',', '<', '>', '.', '_', '(', ')', '$', ':', '%', '[', ']', '#',
-    '?', '&', '^', '!', '*', '/', '|', '-', '\\', '~', '+', '=', '&', '|', '\n', '\t')
+  val operators: List[Char] = List(',', '.', '(', ')', ':', '%', '[', ']', '#',
+    '?', '^', '!', '/', '|', '~', '\n', '\t')
 
-  val characters: Set[Char] = ('a' to 'z').toSet ++ ('A' to 'Z')
+  val characters: Set[Char] = ('a' to 'z').toSet ++ ('A' to 'Z') ++ Set('_')
+  val hexChars: Set[Char] = ('a' to 'f').toSet ++ ('A' to 'F')
   val numbers: Set[Char] = ('0' to '9').toSet
 
-  //TODO: add EOF (when you dont have a string anymore, add EOF)
   val delimitator: List[Char] = List(';', '{', '}')
-  val specialCharacters: List[Char] = List('\n', '\t') //TODO: update list of special chars like \\ , \", \', etc
+  val specialCharacters: List[Char] = List('\n', '\t', '\b', '\f', '\r', '\\', '\'', '\"')
   val quotation_mark: Char = '"'
   val endLine: Char = '\n'
   val star: Char = '*'
@@ -22,13 +22,14 @@ object Language {
   val apostrophe: Char = '\''
   val point: Char = '.'
   val identifiers: Set[Char] = characters ++ numbers
+  val doubleOperators: Set[Char] = Set('&', '|', '<', '>', '+', '-', '=', '*')
   val notstar: Set[Char] = identifiers ++ operators.filter(_ != star) ++ List(space, quotation_mark, apostrophe) ++
-    delimitator ++ specialCharacters
+    delimitator ++ specialCharacters ++ doubleOperators
   val notslash: Set[Char] = identifiers ++ operators.filter(_ != slash) ++ List(space, quotation_mark, apostrophe) ++
-    delimitator ++ specialCharacters
+    delimitator ++ specialCharacters ++ doubleOperators
+  val hexNumbers: Set[Char] = numbers ++ hexChars
 
-  val doubleOperators: Set[Char] = Set('&', '|', '<', '>', '+', '-', '=')
   val allLanguage: Set[Char] = identifiers ++ delimitator ++ List(star, slash, space, quotation_mark, apostrophe) ++
-    operators ++ specialCharacters
+    operators ++ specialCharacters ++ doubleOperators
 
 }
